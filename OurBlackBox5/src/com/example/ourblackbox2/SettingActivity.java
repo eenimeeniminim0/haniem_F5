fa173d457e552e5b85eeb7a12c552bf31de73632
JangMyounghee <zldkszltm2@hanmail.net>
2014-08-24 21:18:37 +0900

    20140824_2

--- OurBlackBox5/src/com/example/ourblackbox2/SettingActivity.java
+++ OurBlackBox5/src/com/example/ourblackbox2/SettingActivity.java
@@ -1,14 +1,18 @@
 package com.example.ourblackbox2;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class SettingActivity extends ActionBarActivity implements OnClickListener {
@@ -16,10 +20,10 @@ public class SettingActivity extends ActionBarActivity implements OnClickListene
 	private Button bQuality;
 	private Button bResolution;
 	private Button bTerm;
-	private Button bLED;
+	private ToggleButton bLED;
 	private Button bAudio;
 	//private Button bBack;
-	
+	private Context context; 
 
 	
 	@Override
@@ -31,17 +35,22 @@ public class SettingActivity extends ActionBarActivity implements OnClickListene
 		//WindowManager.LayoutParams.FLAG_FULLSCREEN);//화면풀스크린
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//화면 가로로 설정
 		
-        setContentView(R.layout.setting_activity);//레코딩 레이아웃
+        setContentView(R.layout.setting_activity);
 		Toast.makeText(getApplicationContext(), "레코딩액티비티 시작", Toast.LENGTH_SHORT).show();
 		
 		bQuality=(Button)findViewById(R.id.button1);
 		bResolution=(Button)findViewById(R.id.button2);
 		bTerm=(Button)findViewById(R.id.button3);
-		bLED=(Button)findViewById(R.id.button4);
+		bLED=(ToggleButton)findViewById(R.id.button4);
 		bAudio=(Button)findViewById(R.id.button5);
 		
-			}
+		bLED.setOnClickListener(this);
 
+	}
+	
+//	public Notification.Builder setLights (int argb, int onMs, int offMs){
+		
+	//}
 
     @Override
     public void onClick(View v) {
@@ -57,11 +66,37 @@ public class SettingActivity extends ActionBarActivity implements OnClickListene
     	
     	case R.id.button4:
     		
+    		/*
+    		if(bLED.isChecked())
+    		{ 
+    			NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    	        Notification notif = new Notification();
+    	        Notification.Builder noti = new Notification.Builder(context);
+    	        setLights(Color.RED,1000,1000);
+    	        notif.defaults = 0;
+    	        notif.flags = Notification.FLAG_SHOW_LIGHTS;
+    	        nm.notify(0, notif);
+    	        Toast.makeText(getApplicationContext(), "LED 켜짐", Toast.LENGTH_SHORT).show();
+    		}
+    		
+    		else
+    		{
+    			
+    		}
+    		*/
+      		break;
+      		
+    		
     	case R.id.button5:
     
     	}
     }
-    
+    public Notification.Builder  setLights (int argb, int onMs, int offMs){
+		return null;
+    	
+    }
+
+
 
 
 }
