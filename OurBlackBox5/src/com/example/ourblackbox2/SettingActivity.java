f79d260179ad2d120bbec5287fdb089ac5467a77
Hanmyokyung <ccunyday@naver.com>
2014-08-24 21:12:36 +0900

    140824_1
    Command 넣음

--- OurBlackBox5/src/com/example/ourblackbox2/SettingActivity.java
+++ OurBlackBox5/src/com/example/ourblackbox2/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.example.ourblackbox2;
 
 import android.content.pm.ActivityInfo;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -9,16 +10,21 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class SettingActivity extends ActionBarActivity implements OnClickListener {
 	
-	private Button bQuality;
+	private ToggleButton bQuality;
 	private Button bResolution;
 	private Button bTerm;
 	private Button bLED;
 	private Button bAudio;
+	protected static boolean isButtonPushed=false;
 	//private Button bBack;
+	
+	SettingControl remote= new SettingControl();
+	BQualityCommand bQCommand= new BQualityCommand();
 	
 
 	
@@ -34,11 +40,16 @@ public class SettingActivity extends ActionBarActivity implements OnClickListene
         setContentView(R.layout.setting_activity);//레코딩 레이아웃
 		Toast.makeText(getApplicationContext(), "레코딩액티비티 시작", Toast.LENGTH_SHORT).show();
 		
-		bQuality=(Button)findViewById(R.id.button1);
+		remote.setCommand(bQCommand);
+		
+		
+		bQuality=(ToggleButton)findViewById(R.id.button1);
 		bResolution=(Button)findViewById(R.id.button2);
 		bTerm=(Button)findViewById(R.id.button3);
 		bLED=(Button)findViewById(R.id.button4);
 		bAudio=(Button)findViewById(R.id.button5);
+		
+		bQuality.setOnClickListener(this);
 		
 			}
 
@@ -49,19 +60,34 @@ public class SettingActivity extends ActionBarActivity implements OnClickListene
     	switch(v.getId()){
     	
     	case R.id.button1:
+    		//녹화품질
+    		 if(bQuality.isChecked())
+    		 {
+    			 Toast.makeText(this, "녹화품질On", Toast.LENGTH_SHORT).show();
+    	    		remote.selected();
+    		 }
+    		 else
+    		 {
+    			 Toast.makeText(this, "녹화품질Off", Toast.LENGTH_SHORT).show();
+    			 remote.unSelected();
+    			 
+    		 }
+    		break;
     		    		
     	case R.id.button2:
+    		//화면 해상도
     		break;
     		
     	case R.id.button3:
+    		//저장설정
     	
     	case R.id.button4:
-    		
+    		//무선네트워크 설정
     	case R.id.button5:
     
     	}
     }
-    
+
 
 
 }
